9eb197dbe206f32826ae6d4141fa0b28b6ad3e8d
Ying Wang <wangy754@hotmail.com>
2020-08-17 23:46:27 -0700

    Eth Summer 2020 project runs on Ropsten test net, built with truffle and node.js.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eth-todo-list
 eth-todo-list for eth summer 2020
+
+Created a dapp runs on Ropsten testnet. 
+Project link: http://dapp.voipeye.com/
+
+reference: https://github.com/dappuniversity/eth-todo-list
---
+++ test/TodoList.test.js
@@ -0,0 +1,46 @@
+
+const TodoList = artifacts.require("TodoList")
+
+contract('TodoList', (accounts) => {
+    before(async () => {
+        this.todoList = await TodoList.deployed()
+    })
+
+    it('deploys successfully', async () => {
+        const address = await this.todoList.address
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+    })
+
+    it('lists tasks', async () => {
+        const taskCount = await this.todoList.taskCount()
+        const task = await this.todoList.tasks(taskCount)
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.content, 'Check out sjsu.edu')
+        assert.equal(task.completed, false)
+        assert.equal(taskCount.toNumber(), 1)
+    })
+
+    it('created tasks', async () => {
+        const result = await this.todoList.createdTask('A new task')
+        const taskCount = await this.todoList.taskCount()
+        assert.equal(taskCount, 2)
+      //  console.log(result)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 2)
+        assert.equal(event.content, 'A new task')
+        assert.equal(event.completed, false)
+    })
+
+    it('toggle task completion', async () => {
+        const result = await this.todoList.toggleCompleted(1)
+        const task = await this.todoList.tasks(1)
+        assert.equal(task.completed, true)
+      //  console.log(result)
+        assert.equal(task.id.toNumber(), 1)
+        assert.equal(task.completed, true)
+    })
+
+}) 
